0083622931875c066d04b4ecb6a95ee6bc6e2fb6
Dmitry <manyakindima@gmail.com>
2024-06-01 20:26:32 +0300

    LaunchScreen

---
+++ app/src/main/java/com/radlance/fooddelivery/presentation/LaunchScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.radlance.fooddelivery.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.radlance.fooddelivery.R
+
+@SuppressLint("CustomSplashScreen")
+class LaunchScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@LaunchScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
--- app/src/main/java/com/radlance/fooddelivery/presentation/MainActivity.kt
+++ app/src/main/java/com/radlance/fooddelivery/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.radlance.fooddelivery
+package com.radlance.fooddelivery.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.radlance.fooddelivery.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
