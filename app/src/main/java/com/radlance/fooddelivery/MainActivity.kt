8777a6cb22c0cb1902e113e78cddd80708ceff91
Dmitry <manyakindima@gmail.com>
2024-06-01 18:05:51 +0300

    Initial commit

---
+++ app/src/main/java/com/radlance/fooddelivery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.radlance.fooddelivery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
